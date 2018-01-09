e772de02f0764706bcb7092950c1a378d5f303a7
Ivan Ramos and Joel Adams <joel@>
2018-01-09 11:07:45 -0800

    add show definitions on click, hide definitions on additonal click

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,127 @@
 $(document).ready(function() {
+  $("h2.javascript").click(function() {
+      $("h2.javascript").hide();
+      $("p.javascript").show();
+    });
+  $("p.javascript").click(function() {
+      $("p.javascript").hide();
+      $("h2.javascript").show();
+    });
+  $("h2.operators").click(function() {
+      $("h2.operators").hide();
+      $("p.operators").show();
+    });
+  $("p.operators").click(function() {
+      $("p.operators").hide();
+      $("h2.operators").show();
+    });
+  $("h2.variables").click(function() {
+      $("h2.variables").hide();
+      $("p.variables").show();
+    });
+  $("p.variables").click(function() {
+      $("p.variables").hide();
+      $("h2.variables").show();
+    });
+  $("h2.vnc").click(function() {
+      $("h2.vnc").hide();
+      $("p.vnc").show();
+    });
+  $("p.vnc").click(function() {
+      $("p.vnc").hide();
+      $("h2.vnc").show();
+    });
+  $("h2.functions").click(function() {
+      $("h2.functions").hide();
+      $("p.functions").show();
+    });
+  $("p.functions").click(function() {
+      $("p.functions").hide();
+      $("h2.functions").show();
+    });
+  $("h2.methods").click(function() {
+      $("h2.methods").hide();
+      $("p.methods").show();
+    });
+  $("p.methods").click(function() {
+      $("p.methods").hide();
+      $("h2.methods").show();
+    });
+  $("h2.arguments").click(function() {
+      $("h2.arguments").hide();
+      $("p.arguments").show();
+    });
+  $("p.arguments").click(function() {
+      $("p.arguments").hide();
+      $("h2.arguments").show();
+    });
+  $("h2.parameters").click(function() {
+      $("h2.parameters").hide();
+      $("p.parameters").show();
+    });
+  $("p.parameters").click(function() {
+      $("p.parameters").hide();
+      $("h2.parameters").show();
+    });
+  $("h2.return").click(function() {
+      $("h2.return").hide();
+      $("p.return").show();
+    });
+  $("p.return").click(function() {
+      $("p.return").hide();
+      $("h2.return").show();
+    });
+  $("h2.chaining").click(function() {
+      $("h2.chaining").hide();
+      $("p.chaining").show();
+    });
+  $("p.chaining").click(function() {
+      $("p.chaining").hide();
+      $("h2.chaining").show();
+    });
+  $("h2.strings").click(function() {
+      $("h2.strings").hide();
+      $("p.strings").show();
+    });
+  $("p.strings").click(function() {
+      $("p.strings").hide();
+      $("h2.strings").show();
+    });
+  $("h2.booleans").click(function() {
+      $("h2.booleans").hide();
+      $("p.booleans").show();
+    });
+  $("p.booleans").click(function() {
+      $("p.booleans").hide();
+      $("h2.booleans").show();
+    });
+  $("h2.undefined").click(function() {
+      $("h2.undefined").hide();
+      $("p.undefined").show();
+    });
+  $("p.undefined").click(function() {
+      $("p.undefined").hide();
+      $("h2.undefined").show();
+    });
+  $("h2.nan").click(function() {
+      $("h2.nan").hide();
+      $("p.nan").show();
+    });
+  $("p.nan").click(function() {
+      $("p.nan").hide();
+      $("h2.nan").show();
+    });
+  $("h2.comments").click(function() {
+      $("h2.comments").hide();
+      $("p.comments").show();
+    });
+  $("p.comments").click(function() {
+      $("p.comments").hide();
+      $("h2.comments").show();
+    });
+
+
+
+
 
 });
